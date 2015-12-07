1cb06c7d62a8743c0515c12dc94080f578a9d019
António Lima <tocalagarto93@gmail.com>
2015-12-07 18:11:02 +0000

    - trying a bottom-up recursive solution

--- day_07/day_07.py
+++ day_07/day_07.py
@@ -2,43 +2,32 @@
 import sys
 import numpy as np
 
-def get_label_value(label):
-
-    # a label must wait to have a value before it is used
-    if label not in wires:
-        return -1
-
-    try:
-        # if it's a number it is fine
-        value = int(label)
-    except ValueError:
-        # if it's a label we have to check its value
-        value = wires[label]
-
-    # guarrantee that it's an unsgned 16-bit
-    return np.array([value], dtype="uint16")[0]
-
 def set_label_value(label, value):
 
     # guarrantee that it's an unsgned 16-bit
-    wires[label] = np.array([value], dtype="uint16")[0]
+    wire_values[label] = np.array([value], dtype="uint16")[0]
 
-if __name__ == "__main__":
+def get_label_value(label):
 
-    input_file = open(sys.argv[1])
-    input_lines = input_file.readlines()
+    # determine type of instruction
+    instruction_parts = wire_graph[label]
+    num_parts = len(instruction_parts)
 
-    wires = {}
+    if num_parts == 1:
+        try:
+            # if it's a number it is fine
+            value = int(label)
+        except ValueError:
+            # if it's a label we have to check its value
+            if label in wire_values.keys():
+                value = wire_values[label]
+            else:
+                value = get_label_value(label)
 
-    for line in input_lines:
+        # guarrantee that it's an unsgned 16-bit
+        return np.array([value], dtype="uint16")[0]
 
-        # split the instruction
-        instruction = line.lstrip().rstrip()
-        instruction_parts = instruction.split(" ")
-        instruction_parts.remove("->")
-
-        # determine type of instruction
-        num_parts = len(instruction_parts)
+    else:
 
         # assignment instruction (123 -> x)
         if num_parts == 2:
@@ -75,5 +64,36 @@ if __name__ == "__main__":
         print(value)
         set_label_value(target_label, value)
 
-    print(wires)
-    print(wires["a"])
+
+if __name__ == "__main__":
+
+    input_file = open(sys.argv[1])
+    input_lines = input_file.readlines()
+
+    wire_values = {}
+    wire_graph = {}
+
+    for line in input_lines:
+
+        # split the instruction
+        instruction = line.lstrip().rstrip()
+        instruction_parts = instruction.split(" ")
+        instruction_parts.remove("->")
+
+        wire_graph[ instruction_parts[-1] ] = instruction_parts[:-1]
+
+        # initial values for netwrok flow are given by "nice" assignments
+        num_parts = len(instruction_parts)
+        if num_parts == 2:
+            try:
+                # if it's a number it is fine
+                value = np.array([int(instruction_parts[0])], dtype="uint16")[0]
+                target_label = instruction_parts[1]
+                wire_values[target_label] = value
+            except ValueError:
+                continue
+
+    print(get_label_value("a"))
+
+    print(wire_values)
+    print(wire_graph)
