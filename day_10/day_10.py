147ea98d4eaf1363f8536aabdbbf009a757a8882
António Lima <tocalagarto93@gmail.com>
2015-12-10 12:14:28 +0000

    - done

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -1,5 +1,31 @@
 import sys
 
+def say_it(number):
+
+    number_occurrences = []
+    cur_char = number[0]
+    cur_char_counter = 0
+
+    for char in number:
+        if char == cur_char:
+            cur_char_counter += 1
+        else:
+            # store the previous count
+            number_occurrences.append([cur_char_counter, cur_char])
+
+            # start the next count
+            cur_char = char
+            cur_char_counter = 1
+
+    # store the last count
+    number_occurrences.append([cur_char_counter, cur_char])
+
+    say_it_result = ''
+    for char_count in number_occurrences:
+        say_it_result += str(char_count[0]) + str(char_count[1])
+
+    return say_it_result
+
 
 if __name__ == "__main__":
 
@@ -9,32 +35,10 @@ if __name__ == "__main__":
     for line in input_lines:
 
         # ignore whitespace
-        line = line.lstrip().rstrip()
+        number = line.lstrip().rstrip()
 
-        print('line', line)
+        for i in range(40):
+            number = say_it(number)
 
-        number_occurrences = []
-        cur_char = line[0]
-        cur_char_counter = 0
+        print("final_result", len(number))
 
-        for char in line:
-            if char == cur_char:
-                cur_char_counter += 1
-            else:
-                # store the previous count
-                number_occurrences.append([cur_char_counter, cur_char])
-
-                # start the next count
-                cur_char = char
-                cur_char_counter = 1
-
-        # store the last count
-        number_occurrences.append([cur_char_counter, cur_char])
-
-        print('number_occurrences', number_occurrences)
-
-        resulting_string = ''
-        for char_count in number_occurrences:
-            resulting_string += str(char_count[0]) + str(char_count[1])
-
-        print('resulting_string', resulting_string, '\n')
